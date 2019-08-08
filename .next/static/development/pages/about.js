4898eadac58e00808eb3a9c187d32d1f85a332b2
Robin Hartley <robert.taku.hartley@gmail.com>
2019-08-08 07:05:22 -0700

    first commit

---
+++ .next/static/development/pages/about.js
@@ -0,0 +1,93 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static\\development\\pages\\about.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fabout&absolutePagePath=C%3A%5CUsers%5CHARTLEY%5CDesktop%5CRobert-CodeMode%5CReact%5CNext%5Chello-next%5Cpages%5Cabout.js!./":
+/*!*******************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fabout&absolutePagePath=C%3A%5CUsers%5CHARTLEY%5CDesktop%5CRobert-CodeMode%5CReact%5CNext%5Chello-next%5Cpages%5Cabout.js ***!
+  \*******************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/about", function() {
+      var page = __webpack_require__(/*! ./pages/about.js */ "./pages/about.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/about.js */ "./pages/about.js", function() {
+          if(!next.router.components["/about"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/about.js */ "./pages/about.js")
+          next.router.update("/about", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_829b10deddf10e1653a8 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_829b10deddf10e1653a8 */ "dll-reference dll_829b10deddf10e1653a8"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return About; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Users\\HARTLEY\\Desktop\\Robert-CodeMode\\React\\Next\\hello-next\\pages\\about.js";
+
+function About() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "This is the about page"));
+}
+
+/***/ }),
+
+/***/ 2:
+/*!***********************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fabout&absolutePagePath=C%3A%5CUsers%5CHARTLEY%5CDesktop%5CRobert-CodeMode%5CReact%5CNext%5Chello-next%5Cpages%5Cabout.js ***!
+  \***********************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fabout&absolutePagePath=C%3A%5CUsers%5CHARTLEY%5CDesktop%5CRobert-CodeMode%5CReact%5CNext%5Chello-next%5Cpages%5Cabout.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fabout&absolutePagePath=C%3A%5CUsers%5CHARTLEY%5CDesktop%5CRobert-CodeMode%5CReact%5CNext%5Chello-next%5Cpages%5Cabout.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_829b10deddf10e1653a8":
+/*!*******************************************!*\
+  !*** external "dll_829b10deddf10e1653a8" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_829b10deddf10e1653a8;
+
+/***/ })
+
+},[[2,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=about.js.map
---
+++ .next/static/webpack/static/development/pages/index.js.89d81f8916cef4c21644.hot-update.js
@@ -0,0 +1,16 @@
+webpackHotUpdate("static\\development\\pages\\index.js",[
+/* 0 */
+false,
+/* 1 */
+/*!******************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=C%3A%5CUsers%5CHARTLEY%5CDesktop%5CRobert-CodeMode%5CReact%5CNext%5Chello-next%5Cpages%5Cindex.js ***!
+  \******************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=C%3A%5CUsers%5CHARTLEY%5CDesktop%5CRobert-CodeMode%5CReact%5CNext%5Chello-next%5Cpages%5Cindex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=C%3A%5CUsers%5CHARTLEY%5CDesktop%5CRobert-CodeMode%5CReact%5CNext%5Chello-next%5Cpages%5Cindex.js!./");
+
+
+/***/ })
+])
+//# sourceMappingURL=index.js.89d81f8916cef4c21644.hot-update.js.map
